af8b2e3384ac5518c04f89a7e51cff7539bf4b74
febrizon <orthomyxoz@gmail.com>
2025-08-04 09:43:18 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,163 @@
+const nav = document.getElementById("nav");
+
+window.addEventListener("scroll", function () {
+  scrollposition = this.window.scrollY;
+  if (scrollposition >= 60) {
+    nav.classList.add("nav-dark");
+  } else if (scrollposition <= 60) {
+    nav.classList.remove("nav-dark");
+  }
+});
+
+new Swiper(".card-wrapper", {
+  loop: true,
+  spaceBetween: 30,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+new Swiper(".wrapper-p", {
+  loop: true,
+  spaceBetween: 30,
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+new Swiper(".card-wrapper-a", {
+  loop: true,
+  spaceBetween: 30,
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+new Swiper(".wrapper-g", {
+  loop: true,
+  spaceBetween: 30,
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+const swiper = new Swiper(".wrapperg", {
+  loop: true,
+  spaceBetween: 30,
+
+  autoplay:{
+    delay: 3000,
+    disableOnInteraction : false,
+    pauseOnMouseEnter : true,
+  },
+
+  //pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
